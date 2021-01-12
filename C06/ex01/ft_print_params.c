6aa8a68d260a39a70a800590c88276ca7191761f
Timothee Brandt <brandt.timothee@gmail.com>
2021-01-12 16:54:58 +0100

    ah

---
+++ C06/ex01/ft_print_params.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void ft_putchar(char c);
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putstr(char *str);
+
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	i = 1;
+	while(i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+}
