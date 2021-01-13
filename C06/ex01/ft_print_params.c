1987028c9c9f66963a94afcc22227f70d6fdaf24
Timothee Brandt <brandt.timothee@gmail.com>
2021-01-13 10:17:39 +0100

    ah

--- C06/ex01/ft_print_params.c
+++ C06/ex01/ft_print_params.c
@@ -2,13 +2,12 @@
 #include <stdio.h>
 
 void ft_putchar(char c);
+void ft_putstr(char *str);
 
 void ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
-
-void ft_putstr(char *str);
 
 void ft_putstr(char *str)
 {
